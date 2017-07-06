6912644f56392e380d634d87dc3855d91a0372a2
David Hodgkinson <lelunedor@gmail.com>
2017-07-06 10:30:32 +0100

    Move android locations due to brew switching to cask

--- android/android.zsh
+++ android/android.zsh
@@ -1,9 +1,9 @@
 #android-ndk location
-export NDK_ROOT=/usr/local/Cellar/android-ndk/r10e #Don't use /opt as the ndk is really unstable when it updates
+export NDK_ROOT=~/Projects/android-ndk-r13b/
 export NDK_CCACHE=/usr/local/opt/ccache/bin/ccache
 
 #location of android utils
-export ANDROID_HOME=/usr/local/opt/android-sdk
+export ANDROID_HOME=~/Projects/android-sdk
 
 #Add android home to path
 export PATH=$PATH:$ANDROID_HOME
