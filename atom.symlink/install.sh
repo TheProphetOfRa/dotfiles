09f00b28f42198445f9ff426a3579fe6bb9d1b04
David Hodgkinson <david.hodgkinson@outplay.com>
2018-05-30 14:46:29 +0100

    Remove python nonsense, add more brew packages and save atom config

---
+++ atom.symlink/install.sh
@@ -0,0 +1,6 @@
+apm install busy-signal
+apm install intentions
+apm install linter-ui-default
+apm install linter
+apm install linter-pylint
+apm install pretty-json
--- python/aliases.zsh
+++
@@ -1,1 +0,0 @@
-alias updatePip="pip freeze --local | grep -v '^\-e' | cut -d = -f 1 | xargs -n1 pip install -U"
